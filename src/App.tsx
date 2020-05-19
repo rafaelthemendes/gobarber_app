f9f1598f5b0c6d91a4218c7f337be203a0349190
Rafael Mendes <rafaelthemendes@gmail.com>
2020-05-19 18:03:06 -0300

    :tada: first commit with basic configuration, lint and react navigation

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+// NOTE: This should be the first import, or the app might crash in production
+import 'react-native-gesture-handler';
+import React from 'react';
+import { StatusBar, View } from 'react-native';
+import Routes from './routes';
+import { NavigationContainer } from '@react-navigation/native';
+
+const App: React.FC = () => (
+  <NavigationContainer>
+    <StatusBar backgroundColor="#312e38" barStyle="light-content" />
+    <View style={{ flex: 1, backgroundColor: '#312e38' }}>
+      <Routes />
+    </View>
+  </NavigationContainer>
+);
+
+export default App;
---
+++ src/pages/SignIn/index.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Container, Title } from './styles';
+import { Image } from 'react-native';
+import logoImg from '../../assets/logo.png';
+
+const SignIn: React.FC = () => {
+  return (
+    <Container>
+      <Image source={logoImg} />
+      <Title>Faça seu logon</Title>
+    </Container>
+  );
+};
+
+export default SignIn;
